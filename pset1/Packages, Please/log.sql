1583729463ab25742bdcdb8af14b0ace29803761
Bruno Del Piero <bdelpiero93@gmail.com>
2024-08-31 21:36:17 +0000

    nits - readability

--- pset1/Packages, Please/log.sql
+++ pset1/Packages, Please/log.sql
@@ -1,4 +1,5 @@
 -- *** The Lost Letter ***
+
 -- when we get the address information for this id, we get smth strange. 
 -- 2 Finnigan Street
 -- There seems to be a typo or smth wrong with the data, as it does not match exactly the expected Varsha's directory: 2 Finnegan Street
@@ -21,12 +22,15 @@ where "id" = (
                                         and contents = 'Congratulatory letter'
                         )
         );
+
 -- *** The Devious Delivery ***
+
 -- get the packages with no From address
 select id
 from packages
 where "from_address_id" is null;
--- sinde the previous query returned a single element, we can assume it is our expected package. no we can look where it ended up at
+
+-- since the previous query returned a single element, we can assume it is our expected package. now we can look where it ended up at
 select *
 from addresses
 where id = (
@@ -39,15 +43,19 @@ where id = (
                         )
                         and "action" = 'Drop'
         );
+
 -- get the contents of the package
 select contents
 from packages
 where "from_address_id" is null;
+
 -- *** The Forgotten Gift ***
+
 -- get the old man's address
 select *
 from addresses
 where "address" = '109 Tileston Street';
+
 -- check for packages sent from this address
 -- this retieves only one result, the content of which is Flowers
 select *
@@ -57,6 +65,7 @@ where "from_address_id" = (
                 from addresses
                 where "address" = '109 Tileston Street'
         );
+
 -- who's got the package now?
 -- lets check the scans for the package to see if smth is off
 select *
@@ -70,6 +79,7 @@ where package_id = (
                                 where "address" = '109 Tileston Street'
                         )
         );
+
 -- so, the packaged was dropped at a warehouse at 950 Brannon Harris Way an then picked again.. but has not been dropped anywhere since that
 -- this means the package is in hands of the driver
 -- lets get the latest scan and use that driver id to get the drivers data
@@ -90,6 +100,7 @@ where id = (
                 order by timestamp desc
                 limit 1
         );
+        
 -- we could maybe do the same subquery using max on timestamp instead of order by
 select "driver_id",
         "timestamp"
